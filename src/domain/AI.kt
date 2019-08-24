bf8af2cedf809439f8cddd3c1c2503467c42b725
Alexander Novik <alexander.novik@banuba.com>
2019-08-24 22:56:13 +0300

    added heuristic algorithm

--- src/domain/AI.kt
+++ src/domain/AI.kt
@@ -10,27 +10,27 @@ object AI {
     fun findBestPosition(board: Board, seed: Seed): Position {
         currentSeed = seed
         oppositeSeed = seed.reverse()
-        return findMinMax(board, seed).position
+        return findMinMax(board, seed, 2).position
     }
 
-    private fun findMinMax(board: Board, seed: Seed): Score {
+    private fun findMinMax(board: Board, seed: Seed, deep: Int): Score {
         var bestScore = if (seed == currentSeed) Int.MIN_VALUE else Int.MAX_VALUE
         var bestPosition = Position(0, 0)
 
-        if (board.getStatus().isFinished) {
-            bestScore = calculate(board)
+        if (board.getStatus().isFinished || deep == 0) {
+            bestScore = calculateHeuristic(board)
         } else {
             for (position in board.getEmptyPositions()) {
                 val copiedBoard = board.copy(gameTable = board.gameTable.toMutableMap())
                 copiedBoard.setPosition(position, seed)
                 if (seed == currentSeed) {
-                    val value = findMinMax(copiedBoard, oppositeSeed).value
+                    val value = findMinMax(copiedBoard, oppositeSeed, deep - 1).value
                     if (value > bestScore) {
                         bestScore = value
                         bestPosition = position
                     }
                 } else {
-                    val value = findMinMax(copiedBoard, currentSeed).value
+                    val value = findMinMax(copiedBoard, currentSeed, deep - 1).value
                     if (value < bestScore) {
                         bestScore = value
                         bestPosition = position
@@ -41,11 +41,15 @@ object AI {
         return Score(bestPosition, bestScore)
     }
 
-    // TODO: implement evristic for the lines
     private fun calculate(board: Board): Int {
         if (board.getWinner() == currentSeed) return 100
         if (board.getWinner() == oppositeSeed) return -100
         return 0
+    }
+
+    private fun calculateHeuristic(board: Board): Int {
+        // calculating sum Heuristics for all 8 lines : 3 strings, 3 rows, 2 diagonals
+        return board.evaluateLines(currentSeed, oppositeSeed)
     }
 
     data class Score(
--- src/domain/Board.kt
+++ src/domain/Board.kt
@@ -79,6 +79,72 @@ data class Board(val size: Int = 3, val gameTable: MutableMap<Position, Seed>) {
         }
     }
 
+    fun evaluateLines(ourSeed: Seed, oppSeed: Seed): Int {
+        var score = 0
+        lines.forEach {
+            score += evaluateLine(it, ourSeed, oppSeed)
+        }
+        return score
+    }
+
+    /**
+     * Heuristic fun for 3 positions
+     *
+     * @return +100, +10, +1 for 3-, 2-, 1- in line seeds ourSeed.
+     * -100, -10, -1 for 3-, 2-, 1- in line seeds  oppSeed.
+     * 0 if line contains the both: X и O
+     */
+    private fun evaluateLine(line: Line, ourSeed: Seed, oppSeed: Seed): Int {
+        var score = 0
+
+        val cell1 = gameTable[line.positions[0]]
+        val cell2 = gameTable[line.positions[1]]
+        val cell3 = gameTable[line.positions[2]]
+
+        if (cell1 == ourSeed) {
+            score = 1
+        } else if (cell1 == oppSeed) {
+            score = -1
+        }
+
+        if (cell2 == ourSeed) {
+            if (score == 1) {   // cell1 is ourSeed
+                score = 10
+            } else if (score == -1) {  // cell1 is oppSeed
+                return 0
+            } else {  // cell1 is empty
+                score = 1
+            }
+        } else if (cell2 == oppSeed) {
+            if (score == -1) { // cell1 is oppSeed
+                score = -10
+            } else if (score == 1) { // cell1 is ourSeed
+                return 0
+            } else {  // cell1 is empty
+                score = -1
+            }
+        }
+
+        if (cell3 == ourSeed) {
+            if (score > 0) {  // cell1 and/or cell2 is ourSeed
+                score *= 10
+            } else if (score < 0) {  // cell1 and/or cell2 is oppSeed
+                return 0
+            } else {  // cell1 and cell2 are empty
+                score = 1
+            }
+        } else if (cell3 == oppSeed) {
+            if (score < 0) {  // cell1 and/or cell2 is oppSeed
+                score *= 10
+            } else if (score > 1) {  // cell1 and/or cell2 is ourSeed
+                return 0
+            } else {  // cell1 and cell2 are empty
+                score = -1
+            }
+        }
+        return score
+    }
+
     val lines = arrayOf(
         Line(arrayListOf(Position(0, 2), Position(1, 2), Position(2, 2))),   // top line
         Line(arrayListOf(Position(0, 1), Position(1, 1), Position(2, 1))),   // middle line
